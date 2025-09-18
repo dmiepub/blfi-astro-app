94e3a9a44d964cd11331d161a1259b7e1bd4db21
dmiepub <dmiepub.dev@gmail.com>
2025-09-18 15:25:00 +0530

    Resolve key issues

--- public/scripts/registerClient.js
+++ public/scripts/registerClient.js
@@ -3,8 +3,12 @@
 // ✅ Load Supabase from CDN
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = import.meta.env.PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+// ⚡ Replace with your environment values
+const SUPABASE_URL = window.ENV.SUPABASE_URL;
+const SUPABASE_ANON_KEY = window.ENV.SUPABASE_ANON_KEY;
+
+//const SUPABASE_URL = import.meta.env.PUBLIC_SUPABASE_URL;
+//const SUPABASE_ANON_KEY = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
